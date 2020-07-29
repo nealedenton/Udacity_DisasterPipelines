8b45e6c206c7fc049216d3d383c04f39b74b7f37
nealedenton <nealedenton@gmail.com>
2020-07-29 21:01:10 +0000

    Completed load_data function.  Resolved issue with database file path

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -26,6 +26,13 @@ from sklearn.metrics import classification_report #multilabel_confusion_matrix
 from sklearn.model_selection import GridSearchCV
 
 def load_data(database_filepath):
+    # load data from database
+    create_engine_argument = 'sqlite:///{}'.format(database_filepath)
+    print(create_engine_argument)
+    engine = create_engine(create_engine_argument)
+    df = pd.read_sql_table('messages', engine)
+    print(df.shape)
+    return df
     pass
 
 
@@ -48,21 +55,22 @@ def save_model(model, model_filepath):
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
+        print(dir())
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
         print('Building model...')
-        model = build_model()
+        #model = build_model()
         
         print('Training model...')
-        model.fit(X_train, Y_train)
+        #model.fit(X_train, Y_train)
         
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        #evaluate_model(model, X_test, Y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+        #save_model(model, model_filepath)
 
         print('Trained model saved!')
 
